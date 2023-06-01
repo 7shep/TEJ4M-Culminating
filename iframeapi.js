5edde8beb23a11854c048eb9eee1a54d6a619434
Alex <sheppenguin@gmail.com>
2023-06-01 15:03:55 -0400

    END OF DAY 1 BABY!

--- iframeapi.js
+++ iframeapi.js
@@ -1,3 +1,15 @@
+//Imports
+
+var express = require('express');
+var request = require('request');
+var cors = require('cors');
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+
+
+var client_id = 'id (put in json)';
+var client_secret = 'secret (put in json)';
+var redirect_uri = 'https://7shep.github.com/TEJ4M-Culminating/loginsuccess.html/';
 
 //Called when the browser has loaded the iFrame API script - the window.onSpotifyIframeApiReady is called. 
 //Signals to app the its safe to rely on the methods in the iFrame API
