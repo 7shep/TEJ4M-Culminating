6e10fd13f1789a51c3998a8263195fee1f2df656
Alex <sheppenguin@gmail.com>
2023-06-02 14:18:36 -0400

    added package.json (hidden for security reasons) + other

--- iframeapi.js
+++ iframeapi.js
@@ -1,20 +1,20 @@
 //Imports
 
-var express = require('express');
-var request = require('request');
-var cors = require('cors');
-var querystring = require('querystring');
-var cookieParser = require('cookie-parser');
-
-
-var client_id = 'id (put in json)';
-var client_secret = 'secret (put in json)';
+//var express = require('express');
+//var request = require('request');
+//var cors = require('cors');
+//var querystring = require('querystring');
+//var cookieParser = require('cookie-parser');
+const CLIENT = require('./package.json');
+//NOTE TO SELF! PUT CLIENT ID AND CLIENT SECRET IN JSON!!
+var client_id = CLIENT.client_id;
+var client_secret = CLIENT.client_secret;
 var redirect_uri = 'https://7shep.github.com/TEJ4M-Culminating/loginsuccess.html/';
 
 //Called when the browser has loaded the iFrame API script - the window.onSpotifyIframeApiReady is called. 
 //Signals to app the its safe to rely on the methods in the iFrame API
-window.onSpotifyIframeApiReady = (IFrameAPI) => {
-    const element = document.getElementById('embed-iframe');
+onSpotifyIframeApiReady = (IFrameAPI) => {
+    const element = document.getElementById('iframe');
     const options = {
         uri: ''
     };
