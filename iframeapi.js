8c1c77991f83765d9e82763c32dda149d95ab3c8
Alex <sheppenguin@gmail.com>
2023-06-06 14:55:46 -0400

    DAY 4!!!

--- iframeapi.js
+++ iframeapi.js
@@ -1,35 +1,3 @@
-//Imports
-
-const CLIENT = require('./package.json');
-var client_id = CLIENT.client_id;
-var client_secret = CLIENT.client_secret;
-var redirect_uri = 'http://localhost:3000/callback';
-
-/**
- * This is an example of a basic node.js script that performs
- * the Authorization Code oAuth2 flow to authenticate against
- * the Spotify Accounts.
- *
- * For more information, read
- * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
- */
-
-var express = require('express');
-var bodyParser = require('body-parser')
-const data = express();
-
-data.use(bodyParser.urlencoded({extended: true}));
-
-  var server = data.listen(3500, () => {
-  var host = server.address().address
-  var port = server.address().port
-  console.log("Example app listening at http://%s:%s", host, port);
-})
-
-//When Submit is Clicked!
-data.post('/spotify-login', () => {
-  console.log('here');
-});
-
-
-
+const CLIENT = require('/package.json');
+const clientid = CLIENT.client_id;
+const clientsecret = CLIENT.client_secret;
