95313313e6559361ed066f1fab763bce8dabaa20
Alex <sheppenguin@gmail.com>
2023-06-08 15:00:33 -0400

    day 6?

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const data = express();
+const bodyParser = require('body-parser');
+
+
+data.use(bodyParser.urlencoded({extended: true}));
+
+  var server = data.listen(5550, () => {
+  var host = server.address().address
+  var port = server.address().port
+  console.log("SHEPFM listening at http://%s:%s", host, port);
+})
+
+data.get('/loginsuccess.html', async (req, res) => {
+    console.log('here');
+    res.redirect('http://127.0.0.1:5500/loginsuccess.html');
+    const logData = req.body.log;
+    console.log(logData);
+});
+
--- topartists.js
+++ topartists.js
@@ -2,28 +2,35 @@ const axios = require('axios');
 const CLIENT = require('./package.json');
 const clientid = CLIENT.client_id;
 const clientsecret = CLIENT.client_secret;
+const TOKEN = "https://accounts.spotify.com/api/token";
+
 
 //Filler for now (just my account, in the future this will be anyones account)
-var access_token = "BQDYDAl3RN1LCNPqK9mlGSGmmQ-UK7RMHfqW7-cs-DI5q526iAVPW04EI07KrpbqI5xAHGGT1InaVATOx9zUAQMNVyuptB2aIikjo7j1ZIoY_fUVxEGLasx5SEkZJM7yB2AzXuaAv3SvZVSUoh4vF0eo0s4v6LxMD-oYNWCB75O8PLjpwhPLbGndHOJqN3tTVtVkRk_b7Al23TkI";
+var access_token = "BQBAqMrmuJMkoNPcCNb9YhNRlZc57OQwGvbr8SMD91daZDB9k6yP2dG6l3IMSw2QAyOAvMHwN6JsDh1lBhi5WiiGyp7F5x6Xh7tkY3H0q03wjy-9vgQhULlE7INTNrvLMaDQJQXMY3V9SY5bFI7CrHxRtLsHoVQ-yV1QY6b6hByPedGW6S5kjDxZlGfX7xv_BuELlu7IWvuguk3i";
 
-axios({
-    //GET method
-    method: 'GET',
-    //Tells axios to GET the info from spotify api
-    url: 'https://api.spotify.com/v1/me/top/artists',
-    //Headers for Access Token
-    headers: {
-        Authorization: `Bearer ${access_token}`
-    },
-    //Parameters for what to grab, short term is last 4 weeks, medium term is last 6 months, limit is limit of artists
-    params: {
-        time_range: 'medium_term',
-        limit: 5
-    }
-})
-    .then(response => {
-        const topArtists = response.data.items;
-        console.log(topArtists);
-    }) .catch(error => {
-        console.error(error);
-    });
+function topArtists(access_token) {
+    axios({
+        //GET method
+        method: 'GET',
+        //Tells axios to GET the info from spotify api
+        url: 'https://api.spotify.com/v1/me/top/artists',
+        //Headers for Access Token
+        headers: {
+            Authorization: `Bearer ${access_token}`
+        },
+        //Parameters for what to grab, short term is last 4 weeks, medium term is last 6 months, limit is limit of artists
+        params: {
+            time_range: 'medium_term',
+            limit: 3
+        }
+    })
+        .then(response => {
+            const topArtists = response.data.items;
+            console.log(topArtists);
+        }) .catch(error => {
+            console.error(error);
+        });
+}
+
+topArtists(access_token);
+
