f7b915b494ed2fab6088f1760141cc1b8a4bf144
Alex <sheppenguin@gmail.com>
2023-06-13 15:04:28 -0400

    grr

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const axios = require('axios');
 const CLIENT = require('./package.json');
 const clientid = CLIENT.client_id;
 const clientsecret = CLIENT.client_secret;
-const TOKEN = "https://accounts.spotify.com/api/token";
-let tokenGrabbed = false;
-data.use(bodyParser.urlencoded({extended: true}));
 
-  var server = data.listen(5550, () => {
+data.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+var server = data.listen(5550, () => {
   var host = server.address().address
   var port = server.address().port
   console.log("SHEPFM listening at http://%s:%s", host, port);
@@ -17,27 +18,47 @@ data.use(bodyParser.urlencoded({extended: true}));
 
 data.get('/loginsuccess.html', async (req, res) => {
 
-    //Took ages to fine but I did :)
-    const token = req.query.code;
-    console.log('Access Token: ' + token);
-    res.redirect('http://127.0.0.1:5500/loginsuccess.html');
-    getTopTracks(token);
+  //Took ages to fine but I did :)
+  axios.post('https://accounts.spotify.com/api/token', null, {
+      headers: {
+        Authorization: 'Basic ' + Buffer.from(clientid + ":" + clientsecret).toString('base64'),
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      data: 'grant_type=client_credentials'
+    }).then(tokenResponse => {
+      const token = tokenResponse.data.access_token;
+      console.log(token);
 
-    
+      return getTopTracks(token);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+
+
+  //console.log('Access Token: ' + token);
+  res.redirect('http://127.0.0.1:5500/loginsuccess.html');
+
+
+
 });
 
-function getTopTracks(token) {
-  axios({
-    method: 'GET',
-    url: 'https://api.spotify.com/v1/me/top/artists',
-    headers: {
-      Authorization: `Bearer ${token}`
-    },
-    params: {
-      time_range: 'short_term',
-      limit: 3
+async function getTopTracks(token) {
+  const response = await axios.get(
+    'https://api.spotify.com/v1/me/top/artists', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params: {
+        time_range: 'short_term',
+        limit: '3'
+      },
+      data: 'grant_type=client_top_artists'
     }
-
-  })
+  ).then(response => {
+    const topTracks = response.body;
+    console.log(topTracks);
+  }).catch(error => {
+    console.error(error);
+  });
 }
-
