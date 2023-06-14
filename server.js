542239c140feb32c0ab2e205f1f067d6d96173f0
Alex <sheppenguin@gmail.com>
2023-06-14 14:55:36 -0400

    this isnt going well at ALL

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 const CLIENT = require('./package.json');
 const clientid = CLIENT.client_id;
 const clientsecret = CLIENT.client_secret;
-
+const redirecturi = 'http://127.0.0.1:5500/loginsuccess';
 data.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -17,48 +17,42 @@ var server = data.listen(5550, () => {
 })
 
 data.get('/loginsuccess.html', async (req, res) => {
-
-  //Took ages to fine but I did :)
-  axios.post('https://accounts.spotify.com/api/token', null, {
-      headers: {
-        Authorization: 'Basic ' + Buffer.from(clientid + ":" + clientsecret).toString('base64'),
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      data: 'grant_type=client_credentials'
-    }).then(tokenResponse => {
-      const token = tokenResponse.data.access_token;
-      console.log(token);
-
-      return getTopTracks(token);
+  var accessToken = req.query.code;
+  
+  axios.post('https://api.spotify.com/api/token', {
+    form: {
+      code: accessToken,
+      redirect_uri: redirecturi,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer.from(clientid + ':' + clientsecret).toString('base64')),
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    json: true
     })
-    .catch(error => {
-      console.error(error);
-    });
 
-
-  //console.log('Access Token: ' + token);
-  res.redirect('http://127.0.0.1:5500/loginsuccess.html');
-
-
-
+  //await getTopArtists(accessToken);
+ 
 });
 
-async function getTopTracks(token) {
-  const response = await axios.get(
-    'https://api.spotify.com/v1/me/top/artists', {
+async function getTopArtists(accessToken) {
+  try {
+    const response = await axios({
+      url: 'https://api.spotify.com/v1/me/top/artists',
+      method: 'GET',
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${accessToken}`,
       },
       params: {
         time_range: 'short_term',
         limit: '3'
-      },
-      data: 'grant_type=client_top_artists'
-    }
-  ).then(response => {
-    const topTracks = response.body;
-    console.log(topTracks);
-  }).catch(error => {
+      }
+    });
+
+    const topArtists = response.data;
+    console.log(topArtists);
+  } catch (error) {
     console.error(error);
-  });
+  }
 }
